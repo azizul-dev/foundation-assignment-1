26089c20c607f0842290430af050d96699814c95
Azizul Islam <abdulazizabdul8822@gmail.com>
2026-09-06 20:55:39 +0600

    solve get day type problem

--- answer.js
+++ answer.js
@@ -12,7 +12,33 @@ function describeValue(value) {
   return type + " | " + result;
 }
 
-console.log(describeValue("hello"));
-console.log(describeValue(0));
-console.log(describeValue(null));
-console.log(describeValue(NaN));
+// console.log(describeValue("hello"));
+// console.log(describeValue(0));
+// console.log(describeValue(null));
+// console.log(describeValue(NaN));
+
+//Question No. 2
+function getDayType(day) {
+  day = day.toLowerCase();
+
+  switch (day) {
+    case "friday":
+    case "saturday":
+      return "Weekend";
+
+    case "sunday":
+    case "monday":
+    case "tuesday":
+    case "wednesday":
+    case "thursday":
+      return "Working Day";
+
+    default:
+      return "Invalid Day";
+  }
+}
+
+console.log(getDayType("Friday"));
+console.log(getDayType("friday"));
+console.log(getDayType("MONDAY"));
+console.log(getDayType("Bandarban"));
