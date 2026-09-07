912f47f971422120c1e9f05efcb2b764b22a7784
Azizul Islam <abdulazizabdul8822@gmail.com>
2026-09-07 07:38:31 +0600

    solve Dhaka CNG fare meter problem

--- answer.js
+++ answer.js
@@ -60,10 +60,38 @@ function validateUsername(username) {
 
   return "Available";
 }
-console.log(validateUsername("rahim123"));
-console.log(validateUsername("ab"));
-console.log(validateUsername("a b"));
-console.log(validateUsername("abcd"));
-console.log(validateUsername("rahim islam"));
-console.log(validateUsername("superadmin99"));
-console.log(validateUsername("Admin_Rahim"));
+// console.log(validateUsername("rahim123"));
+// console.log(validateUsername("ab"));
+// console.log(validateUsername("a b"));
+// console.log(validateUsername("abcd"));
+// console.log(validateUsername("rahim islam"));
+// console.log(validateUsername("superadmin99"));
+// console.log(validateUsername("Admin_Rahim"));
+
+//Question No. 4
+
+function getCngFare(distance, isNight = false, waitingMinutes = 0) {
+  let fare = 50;
+
+  if (distance > 2) {
+    fare = fare + (distance - 2) * 15;
+  }
+
+  fare = fare + waitingMinutes * 2;
+
+  if (isNight) {
+    fare = fare * 1.20;
+  }
+
+  return fare;
+}
+console.log(getCngFare(2));
+console.log(getCngFare(1));
+console.log(getCngFare(5));
+console.log(getCngFare(10));
+console.log(getCngFare(5, false, 10));
+console.log(getCngFare(5, true));
+console.log(getCngFare(5, true, 10));
+
+
+//Question No. 5
